16bcbee82c50e8e754087a9c862e36510ac4beed
Sudhir Mishra <sudhirxps@gmail.com>
2022-07-07 22:39:45 +0530

    yet another try at github actions

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/banking'),
+      dir: require('path').join(__dirname, './coverage/banking-project-work'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
