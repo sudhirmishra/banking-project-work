b7dd954c258facf929897eea78fbd12644448560
Sudhir Mishra <sudhirxps@gmail.com>
2022-07-07 18:55:45 +0530

    application layout added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HistoryComponent } from './history/history.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'transactions', component:HistoryComponent,
+  },
+  {
+    path:'dashboard', component:DashboardComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
